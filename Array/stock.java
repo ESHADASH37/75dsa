9299b6332590dbac592dbbeb6ff39efbfa5bfedf
ESHADASH37 <eshadash407@gmail.com>
2026-08-02 20:50:26 +0530

    Stock Buy and Sell

---
+++ Array/stock.java
@@ -0,0 +1,21 @@
+public class stock {
+    public static int buystockandsell(int[] prices){
+        int minprice =Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<minprice){
+                minprice=prices[i];
+            }
+            else if (prices[i]-minprice>maxprofit){
+                maxprofit =prices[i] - minprice;
+            }
+        }
+        return maxprofit;
+    }
+    public static void main(String args[]){
+        int [] prices={7, 1, 5, 3, 6, 4};
+        System.out.println("Maximum profit : " +buystockandsell(prices));
+
+    }
+    
+}
