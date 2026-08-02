3e1dbb9920bf9d2706bb93c05095833d5312d76c
ESHADASH37 <eshadash407@gmail.com>
2026-08-02 20:28:10 +0530

    Sort an array of 0's 1's and 2's

---
+++ Array/sort012.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+public class sort012 {
+    public static void sortanarray012(int []  arr){
+        int low = 0;
+        int mid = 0;
+        int high = arr.length - 1;
+
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr,low,mid);
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                swap(arr,mid,high);
+                    high--;
+                }
+            }
+        }
+        private static void swap(int[] arr,int i,int j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+        public static void main(String args[]){
+            int[] arr ={2, 0, 2, 1, 1, 0};
+            sortanarray012(arr);
+            System.out.println("Sorted Array:"+Arrays.toString(arr));
+        }
+    }
+    
+
