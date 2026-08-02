42d12cbdcbbe70963cfb87b4146fbe5716f1d1b1
ESHADASH37 <eshadash407@gmail.com>
2026-08-02 22:43:54 +0530

    Rotate matrix by 90 degrees

---
+++ Array/rotate90.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+public class rotate90 {
+    public static void rotatematrixby90(int[][]matrix){
+        int n=matrix.length;
+
+        for(int i =0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int start =0;
+            int end =n-1;
+            while(start<end){
+                int temp = matrix[i][start];
+                matrix[i][start]=matrix[i][end];
+                matrix[i][end]=temp;
+                start++;
+                end--;
+            
+            }
+        }
+    }
+    public static void main(String args[]){
+        int [] [] matrix={
+            {1 , 2 , 3},
+            {4, 5 , 6},
+            {7 , 8 , 9}
+        };
+        rotatematrixby90(matrix);
+        System.out.println("rotated Matrix:");
+        for(int[] row:matrix){
+            System.out.println(Arrays.toString(row));
+        }
+    }
+
+    
+}
